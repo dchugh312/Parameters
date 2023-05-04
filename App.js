ded144420da9dd3f76a91b7be9705eb4590629b8
Divleen Chugh <dchugh121@gmail.com>
2023-05-04 21:55:57 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {
+  useApplicationAndroidInstallReferrer,
+  useApplicationInstallTime,
+  useApplicationAndroidLastUpdateTime,
+} from '@use-expo/application';
 
 export default function App() {
+  const [referrer] = useApplicationAndroidInstallReferrer();
+  const [installTime] = useApplicationInstallTime();
+  const [updateTime] = useApplicationAndroidLastUpdateTime();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ marginBottom: 10 }}>URL: {referrer || '-'}</Text>
+      <Text style={{ marginBottom: 10 }}>
+        Install time: {installTime ? installTime.toString() : '-'}
+      </Text>
+      <Text style={{ marginBottom: 10 }}>
+        Last update time: {updateTime ? updateTime.toString() : '-'}
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
